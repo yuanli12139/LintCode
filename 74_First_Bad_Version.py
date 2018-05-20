e22391796aff71a4f4d2f57400bdaee14e451de4
Yuan Li <yuanli12139@gmail.com>
2018-05-19 23:32:50 -0700

    initial commit

---
+++ 74_First_Bad_Version.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+'''
+74. First Bad Version
+The code base version is an integer start from 1 to n. One day, someone committed a bad version in the code case, so it caused this version and the following versions are all failed in the unit tests. Find the first bad version.
+
+You can call isBadVersion to help you determine which version is the first bad one. The details interface can be found in the code's annotation part.
+
+Example
+Given n = 5:
+
+isBadVersion(3) -> false
+isBadVersion(5) -> true
+isBadVersion(4) -> true
+Here we are 100% sure that the 4th version is the first bad version.
+
+Challenge
+You should call isBadVersion as few as possible.
+
+
+Author: Yuan Li
+Date: 5/19/2018 
+Difficulty: Medium
+'''
+
+"""
+class SVNRepo:
+    @classmethod
+    def isBadVersion(cls, id)
+        # Run unit tests to check whether verison `id` is a bad version
+        # return true if unit tests passed else false.
+You can use SVNRepo.isBadVersion(10) to check whether version 10 is a 
+bad version.
+"""
+
+
+class Solution:
+    """
+    @param: n: An integer
+    @return: An integer which is the first bad version.
+    """
+    def findFirstBadVersion(self, n):
+        # write your code here
+        start, end = 1, n
+        while start + 1 < end:
+            mid = int((start + end) / 2)
+            if SVNRepo.isBadVersion(mid):
+                end = mid
+            else:
+                start = mid
+                
+        if SVNRepo.isBadVersion(start):
+            return start
+        return end
+        
