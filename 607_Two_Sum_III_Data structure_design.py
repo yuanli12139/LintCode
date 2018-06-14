f7b92a5fe652e01103d987ddc36a08b645a2400e
Yuan Li <yuanli12139@gmail.com>
2018-06-13 22:32:20 -0700

    initial commit

---
+++ 607_Two_Sum_III_Data structure_design.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+'''
+607. Two Sum III - Data structure design
+Design and implement a TwoSum class. It should support the following operations: add and find.
+
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+
+Example
+add(1); add(3); add(5);
+find(4) // return true
+find(7) // return false
+
+
+Author: Yuan Li
+Date: 6/12/2018 
+Difficulty: Easy
+'''
+
+class TwoSum:
+    nums = []
+    
+    """
+    @param: number: An integer
+    @return: nothing
+    """
+    def add(self, number):
+        # write your code here
+        self.nums.append(number)
+
+    """
+    @param: value: An integer
+    @return: Find if there exists any pair of numbers which sum is equal to the value.
+    """
+    def find(self, value):
+        # write your code here
+        hashTable = {}
+        for num in self.nums:
+            if value - num in hashTable:
+                return True
+            hashTable[num] = num
+                
+        return False
+
+        
