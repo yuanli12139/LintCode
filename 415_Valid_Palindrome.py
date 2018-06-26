c127bfeeb6f9cdb1873944e9311f5668e5b97f5a
Yuan Li <yuanli12139@gmail.com>
2018-06-25 22:05:14 -0700

    add more description

--- 415_Valid_Palindrome.py
+++ 415_Valid_Palindrome.py
@@ -4,6 +4,9 @@
 415. Valid Palindrome
 Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+For the purpose of this problem, we define empty string as valid palindrome.
+
 Example
 "A man, a plan, a canal: Panama" is a palindrome.
 
