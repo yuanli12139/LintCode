0ea7c30bab666de5666986acd4f2822807ca1841
Yuan Li <yuanli12139@gmail.com>
2018-06-25 23:27:23 -0700

    add clarification

--- 13_Implement_strStr().py
+++ 13_Implement_strStr().py
@@ -6,6 +6,11 @@ For a given source string and a target string, you should output the first index
 
 If target does not exist in source, just return -1.
 
+Clarification
+Do I need to implement KMP Algorithm in a real interview?
+Not necessary. When you meet this problem in a real interview, the interviewer may just want to test your 
+basic implementation ability. But make sure you confirm with the interviewer first.
+
 Example
 If source = "source" and target = "target", return -1.
 
