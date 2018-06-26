5d651ded267d9607ada08605cdd62096938c159b
Yuan Li <yuanli12139@gmail.com>
2018-06-25 21:57:28 -0700

    more description

--- 627_Longest_Palindrome.py
+++ 627_Longest_Palindrome.py
@@ -6,6 +6,8 @@ Given a string which consists of lowercase or uppercase letters, find the length
 
 This is case sensitive, for example "Aa" is not considered a palindrome here.
 
+Assume the length of given string will not exceed 1010.
+
 Example
 Given s = "abccccdd" return 7
 
