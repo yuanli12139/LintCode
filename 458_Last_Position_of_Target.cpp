68dbea45bf1c6876cf6889d17749733574fa9f55
Yuan Li <yuanli12139@gmail.com>
2018-06-26 18:50:39 -0700

    initial commit

---
+++ 458_Last_Position_of_Target.cpp
@@ -0,0 +1,55 @@
+/*
+'''
+458. Last Position of Target
+Find the last position of a target number in a sorted array. Return -1 if target does not exist.
+
+Example
+Given [1, 2, 2, 4, 5, 5].
+
+For target = 2, return 2.
+
+For target = 5, return 5.
+
+For target = 6, return -1.
+
+
+Author: Yuan Li
+Date: 6/26/2018 
+Difficulty: Easy
+*/
+
+class Solution {
+public:
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return: An integer
+     */
+    int lastPosition(vector<int> &nums, int target) {
+        // write your code here
+        if (nums.empty() || nums[0] > target || nums.back() < target) {
+            return -1;
+        }
+        
+        int start = 0, end = nums.size() - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2; // avoid int overflow
+            if (nums[mid] == target) {
+                start = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (nums[end] == target) {
+            return end;
+        }
+        if (nums[start] == target) {
+            return start;
+        }
+        
+        return -1;
+    }
+};
