68904618fc57618c37b1a3ff24de41a0e66dab5c
Yuan Li <yuanli12139@gmail.com>
2018-07-13 21:14:47 -0700

    add clarification

--- 900_Closest_Binary_Search_Tree_Value.py
+++ 900_Closest_Binary_Search_Tree_Value.py
@@ -4,6 +4,9 @@
 900. Closest Binary Search Tree Value
 Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
 
+Given target value is a floating point.
+You are guaranteed to have only one unique value in the BST that is closest to the target.
+
 Example
 Given root = {1}, target = 4.428571, return 1.
 
