9664ea900e59d2f226d7249da234fdf271686306
Yuan Li <yuanli12139@gmail.com>
2018-07-29 13:45:03 -0700

    add clarification

--- 64_Merge_Sorted_Array.cpp
+++ 64_Merge_Sorted_Array.cpp
@@ -2,6 +2,8 @@
 64. Merge Sorted Array
 Given two sorted integer arrays A and B, merge B into A as one sorted array.
 
+You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B. The number of elements initialized in A and B are m and n respectively.
+
 Example
 A = [1, 2, 3, empty, empty], B = [4, 5]
 
