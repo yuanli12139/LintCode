362205d394147e152ed3e58dbfe52992476da9c3
Yuan Li <yuanli12139@gmail.com>
2018-07-29 01:22:34 -0700

    findKth sol

--- 65. Median of two Sorted Arrays.cpp
+++ 65. Median of two Sorted Arrays.cpp
@@ -207,3 +207,48 @@ public:
         return A.getMedian();
     }
 };
+
+
+// divide & conquer - O(log(m + n))
+class Solution {
+public:
+    /*
+     * @param A: An integer array
+     * @param B: An integer array
+     * @return: a double whose format is *.5 or *.0
+     */
+    double findMedianSortedArrays(vector<int> &A, vector<int> &B) {
+        // write your code here
+        int n = A.size() + B.size();
+        
+        if (n % 2 == 0) {
+            return (findKth(A, 0, B, 0, n / 2) + findKth(A, 0, B, 0, n / 2 + 1)) / 2.;
+        }
+        
+        return findKth(A, 0, B, 0, n / 2 + 1);
+    }
+    
+    int findKth(vector<int> &A, int start_A, vector<int> &B, int start_B, int k) {
+        if (start_A >= A.size()) {
+            return B[start_B + k - 1];
+        }
+        
+        if (start_B >= B.size()) {
+            return A[start_A + k - 1];
+        }
+        
+        if (k == 1) {
+            return min(A[start_A], B[start_B]);
+        }
+        
+        // ignore smaller numbers
+        int A_halfKth = start_A + k / 2 - 1 < A.size() ? A[start_A + k / 2 - 1] : INT_MAX;
+        int B_halfKth = start_B + k / 2 - 1 < B.size() ? B[start_B + k / 2 - 1] : INT_MAX;
+        
+        if (A_halfKth < B_halfKth) {
+            return findKth(A, start_A + k / 2, B, start_B, k - k / 2);
+        }
+
+        return findKth(A, start_A, B, start_B + k / 2, k - k / 2);
+    }
+};
