3f0c6e1cffaf3aaa5ebce9030a2f4c303ad20ed2
Yuan Li <yuanli12139@gmail.com>
2018-07-29 19:52:29 -0700

    smaller extra space

--- 547_Intersection_of_Two_Arrays.cpp
+++ 547_Intersection_of_Two_Arrays.cpp
@@ -34,12 +34,22 @@ public:
             return res;
         }
         
+        if (nums1.size() < nums2.size()) {
+            return helper(nums1, nums2);
+        }
+        
+        return helper(nums2, nums1);
+    }
+    
+    vector<int> helper(const vector<int> &smaller, const vector<int> &larger) {
+        vector<int> res;
+        
         unordered_map<int, bool> num2visited;
-        for (int n1 : nums1) {
+        for (int n1 : smaller) {
             num2visited[n1] = true;
         }
         
-        for (int n2 : nums2) {
+        for (int n2 : larger) {
             if (num2visited.count(n2) && num2visited[n2]) {
                 res.push_back(n2);
                 num2visited[n2] = false;
@@ -47,5 +57,5 @@ public:
         }
         
         return res;
-    }
+    } 
 };
