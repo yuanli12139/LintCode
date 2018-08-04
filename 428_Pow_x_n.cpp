f4a66be207a9c67e97cba5aa58b7d01689a4abe6
Yuan Li <yuanli12139@gmail.com>
2018-08-04 14:02:23 -0700

    initial commit

---
+++ 428_Pow_x_n.cpp
@@ -0,0 +1,48 @@
+/*
+428. Pow(x, n)
+Implement pow(x, n).
+
+Example
+Pow(2.1, 3) = 9.261
+Pow(0, 1) = 0
+Pow(1, 0) = 1
+
+Challenge
+O(logn) time
+
+
+Author: Yuan Li
+Date: 8/4/2018 
+Difficulty: Medium
+*/
+
+class Solution {
+public:
+    /*
+     * @param x: the base number
+     * @param n: the power number
+     * @return: the result
+     */
+    double myPow(double x, int n) {
+        // write your code here
+        if (n < 0) {
+            return 1.0 / power(x, -n);
+        }
+        
+        return power(x, n);
+    }
+    
+    double power(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        double tmp = power(x, n / 2);
+        if (n % 2 == 0) {
+            return tmp * tmp;
+        }
+        
+        // when n == INT_MIN, always return x * tmp * tmp => inf
+        return x * tmp * tmp;
+    }
+};
