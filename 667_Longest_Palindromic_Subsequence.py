6fb9e37303c5d7531033d6d7a43a1f6a6af64dfa
Yuan Li <yuanli12139@gmail.com>
2018-08-11 23:20:05 -0700

    sol with O(n) space

--- 667_Longest_Palindromic_Subsequence.py
+++ 667_Longest_Palindromic_Subsequence.py
@@ -45,3 +45,41 @@ class Solution:
                     dp[i][j] = max(dp[i+1][j], dp[i][j-1])
                     
         return dp[0][n-1]
+
+
+# Time: O(n^2)
+# Space: O(n)
+class Solution:
+    """
+    @param s: the maximum length of s is 1000
+    @return: the longest palindromic subsequence's length
+    """
+    def longestPalindromeSubseq(self, s):
+        # write your code here
+        n = len(s)
+        
+        if n == 0:
+            return 0
+            
+        dp0 = [0 for _ in range(n)] # solutions of l
+        dp1 = [0 for _ in range(n)] # solutions of l - 1
+        dp2 = [0 for _ in range(n)] # solutions of l - 2
+        
+        for l in range(1, n + 1):
+            for i in range(0, n - l + 1):
+                j = i + l - 1
+                
+                if i == j:
+                    dp0[i] = 1
+                    continue
+                
+                if s[i] == s[j]:
+                    dp0[i] = dp2[i+1] + 2
+                
+                else:
+                    dp0[i] = max(dp1[i+1], dp1[i])
+                    
+            dp2 = dp1[:]
+            dp1 = dp0[:]
+                    
+        return dp0[0]
