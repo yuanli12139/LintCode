c40c26861ddd612b241b5fb093c95ea40ea13ea4
Yuan Li <yuanli12139@gmail.com>
2018-08-11 22:45:36 -0700

    initial commit

---
+++ 667_Longest_Palindromic_Subsequence.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+'''
+667. Longest Palindromic Subsequence
+Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000.
+
+Example
+Given s = "bbbab" return 4
+One possible longest palindromic subsequence is "bbbb".
+
+
+Author: Yuan Li
+Date: 8/11/2018 
+Difficulty: Medium
+'''
+
+# Time: O(n^2)
+# Space: O(n^2)
+class Solution:
+    """
+    @param s: the maximum length of s is 1000
+    @return: the longest palindromic subsequence's length
+    """
+    def longestPalindromeSubseq(self, s):
+        # write your code here
+        n = len(s)
+        
+        if n == 0:
+            return 0
+            
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        
+        for l in range(1, n + 1):
+            for i in range(0, n - l + 1):
+                j = i + l - 1
+                
+                if i == j:
+                    dp[i][j] = 1
+                    continue
+                
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+                    
+        return dp[0][n-1]
