3c2f32cd552036c861353e77e1813260a749ffad
Yuan Li <yuanli12139@gmail.com>
2018-09-01 21:24:55 -0700

    add O(n) sol using start index

--- 512_Decode_Ways.cpp
+++ 512_Decode_Ways.cpp
@@ -69,3 +69,52 @@ class Solution {
   private:
     unordered_map<string, int> str_ways_;
 };
+
+
+// time: O(n)
+// space: O(n)
+class Solution {
+  public:
+    /**
+     * @param s: a string,  encoded message
+     * @return: an integer, the number of ways decoding
+     */
+    int numDecodings(string &s) {
+        // write your code here
+        if (s.empty()) {
+            return 0;
+        }
+    
+        return helper(s, 0);
+    }
+  
+  private:
+    int helper(const string &s, int start) {
+        if (str_ways_.count(start)) {
+            return str_ways_[start];
+        }    
+        
+        if (s[start] == '0') {
+            return 0;
+        }
+        
+        // single charactr or empty
+        if (start >= s.length() - 1) { 
+            return 1;
+        }
+        
+        // remove the 1st character
+        int n = helper(s, start + 1);
+        
+        // remove the 1st 2 characters
+        if ((s[start] - '0') * 10 + (s[start+1] - '0') <= 26) {
+            n += helper(s, start + 2);
+        }
+        
+        str_ways_[start] = n;
+        return n;
+    }
+    
+  private:
+    unordered_map<int, int> str_ways_;
+};
