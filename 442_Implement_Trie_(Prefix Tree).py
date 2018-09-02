318fbb16747346556fe37544f03b480e8fe3b368
Yuan Li <yuanli12139@gmail.com>
2018-09-02 00:56:52 -0700

    initial commit

---
+++ 442_Implement_Trie_(Prefix Tree).py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+'''
+442. Implement Trie (Prefix Tree)
+Implement a trie with insert, search, and startsWith methods.
+
+Example
+insert("lintcode")
+search("code")
+>>> false
+startsWith("lint")
+>>> true
+startsWith("linterror")
+>>> false
+insert("linterror")
+search("lintcode)
+>>> true
+startsWith("linterror")
+>>> true
+
+Notice
+You may assume that all inputs are consist of lowercase letters a-z.
+
+
+Author: Yuan Li
+Date: 9/1/2018 
+Difficulty: Medium
+'''
+
+class Trie:
+    
+    def __init__(self):
+        # do intialization if necessary
+        self.root = {}
+
+    """
+    @param: word: a word
+    @return: nothing
+    """
+    def insert(self, word):
+        # write your code here
+        p = self.root
+        for c in word:
+            if c not in p:
+                p[c] = {}
+            
+            p = p[c]
+            
+        p['#'] = True
+
+    """
+    @param: word: A string
+    @return: if the word is in the trie.
+    """
+    def search(self, word):
+        # write your code here
+        node = self.find(word)
+        return node is not None and '#' in node 
+
+    """
+    @param: prefix: A string
+    @return: if there is any word in the trie that starts with the given prefix.
+    """
+    def startsWith(self, prefix):
+        # write your code here
+        return self.find(prefix) is not None
+        
+    def find(self, prefix):
+        p = self.root
+        for c in prefix:
+            if c not in p:
+                return None
+                
+            p = p[c]
+            
+        return p
