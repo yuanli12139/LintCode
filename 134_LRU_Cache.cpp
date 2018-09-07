ffe53f032494b152acb9678bd7fc45a0d7767a8d
Yuan Li <yuanli12139@gmail.com>
2018-09-06 23:23:41 -0700

    add doubly linked list sol

--- 134_LRU_Cache.cpp
+++ 134_LRU_Cache.cpp
@@ -11,6 +11,7 @@ Date: 7/23/2018
 Difficulty: Hard
 */
 
+// singly linked list
 class LRUCache {
   public:
     /*
@@ -122,4 +123,73 @@ class LRUCache {
         node->next = nullptr; 
         push_back(node);
     }
+};
+
+
+/*
+Reference
+https://zxi.mytechroad.com/blog/hashtable/leetcode-146-lru-cache/
+
+Date: 9/6/2018
+*/
+
+// doubly linked list
+#include <list>
+
+class LRUCache {
+  public:
+    /*
+    * @param capacity: An integer
+    */
+    LRUCache(int capacity) {
+        // do intialization if necessary
+        capacity_ = capacity;
+    }
+
+    /*
+     * @param key: An integer
+     * @return: An integer
+     */
+    int get(int key) {
+        // write your code here
+        const auto mit = m_.find(key);
+        if (mit == m_.end()) {
+            return -1;
+        }
+        
+        // key exists
+        cache_.splice(cache_.begin(), cache_, mit->second);
+        return mit->second->second;
+    }
+
+    /*
+     * @param key: An integer
+     * @param value: An integer
+     * @return: nothing
+     */
+    void set(int key, int value) {
+        // write your code here
+        const auto mit = m_.find(key);
+        if (mit != m_.end()) { // key exists
+            mit->second->second = value;
+            cache_.splice(cache_.begin(), cache_, mit->second);
+            return;
+        }
+        
+        // key doesn't exist, check capacity first
+        if (m_.size() == capacity_) {
+            const auto &node = cache_.back();
+            m_.erase(node.first);
+            cache_.pop_back();
+        }
+        
+        // add new node to the front
+        cache_.emplace_front(key, value);
+        m_[key] = cache_.begin();
+    }
+    
+  private:
+    int capacity_;
+    list<pair<int, int>> cache_; // {key, value}
+    unordered_map<int, list<pair<int, int>>::iterator> m_; // key -> iterator of node
 };
