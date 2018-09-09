1ae837e95c979190da59414778dc73893eda2278
Yuan Li <yuanli12139@gmail.com>
2018-09-09 15:52:32 -0700

    new sol

--- 829_Word_Pattern_II.py
+++ 829_Word_Pattern_II.py
@@ -6,12 +6,13 @@ Given a pattern and a string str, find if str follows the same pattern.
 
 Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty substring in str.(i.e if a corresponds to s, then b cannot correspond to s. For example, given pattern = "ab", str = "ss", return false.)
 
-You may assume both pattern and str contains only lowercase letters.
-
 Example
 Given pattern = "abab", str = "redblueredblue", return true.
 Given pattern = "aaaa", str = "asdasdasdasd", return true.
 Given pattern = "aabb", str = "xyzabcxzyabc", return false.
+
+Notice
+You may assume both pattern and str contains only lowercase letters.
 
 
 Author: Yuan Li
