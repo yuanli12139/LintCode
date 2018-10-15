e89e930e6f8243b3f2d1024463ab316c93756016
yuanli12139 <yuanli12139@gmail.com>
2018-10-15 12:06:50 -0700

    initial commit

---
+++ 645_Find_the_Celebrity.cpp
@@ -0,0 +1,54 @@
+/*
+645. Find the Celebrity
+Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity. The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
+
+Now you want to find out who the celebrity is or verify that there is not one. The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+
+You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n), your function should minimize the number of calls to knows.
+
+Example
+Given n = 2
+
+2 // next n * (n - 1) lines 
+0 knows 1
+1 does not know 0
+return 1 // 1 is celebrity
+
+Notice
+There will be exactly one celebrity if he/she is in the party. Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
+
+
+Author: Yuan Li
+Date: 10/15/2018 
+Difficulty: Medium
+*/
+
+// Forward declaration of the knows API.
+bool knows(int a, int b);
+
+class Solution {
+public:
+    /**
+     * @param n a party with n people
+     * @return the celebrity's label or -1
+     */
+    int findCelebrity(int n) {
+        // Write your code here
+        int celebrity = 0;
+        for (int i = 1; i < n; ++i) {
+            if (knows(celebrity, i)) {
+                celebrity = i;
+            }
+        }
+        
+        // verify celebrity as there might be no celebrity
+        for (int i = 0; i < n; ++i) {
+            if (celebrity != i && (knows(celebrity, i) || !knows(i, celebrity))) {
+                return -1;
+            }
+        }
+        
+        return celebrity;
+    }
+};
+
