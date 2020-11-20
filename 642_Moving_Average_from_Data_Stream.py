0428eb1f1c74dc91a89c481da6c0fb6ce33e98f0
Yuan Li <yuanli12139@gmail.com>
2020-11-19 16:46:39 -0800

    add deque sol

--- 642_Moving_Average_from_Data_Stream.py
+++ 642_Moving_Average_from_Data_Stream.py
@@ -45,3 +45,39 @@ class MovingAverage:
 # Your MovingAverage object will be instantiated and called as such:
 # obj = MovingAverage(size)
 # param = obj.next(val)
+
+
+'''
+Date: 11/19/2020
+'''
+
+# deque
+class MovingAverage:
+    """
+    @param: size: An integer
+    """
+    def __init__(self, size):
+        # do intialization if necessary
+        from collections import deque
+        self.size = size
+        self.q = deque()
+        self.s = 0
+        
+    """
+    @param: val: An integer
+    @return:  
+    """
+    def next(self, val):
+        # write your code here
+        if len(self.q) == self.size:
+            self.s -= self.q[0]
+            self.q.popleft()
+        
+        self.s += val
+        self.q.append(val)
+        
+        return self.s / len(self.q)
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param = obj.next(val)
